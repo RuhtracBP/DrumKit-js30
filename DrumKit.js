619df7a47661acd07a69c6bd17fbcaf36b585299
Arthur <arthurhcbp@gmail.com>
2021-01-21 18:25:45 -0300

    drum kit

---
+++ DrumKit.js
@@ -0,0 +1,30 @@
+
+
+
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    if (!audio) return;  // if none of the predermited key are pressed
+
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    
+    audio.currentTime = 0; // rewind audio time
+    audio.play();
+    key.classList.add('playing');
+}
+
+function removeTransition(e) {
+    
+    if (e.propertyName !== 'transform') return; //skip it if not a transform
+    
+    this.classList.remove('playing');
+}
+
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+    
+
+window.addEventListener('keydown', playSound);
+
+
+    
